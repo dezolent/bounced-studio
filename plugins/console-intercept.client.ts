8ad936d0be9b8f8bd46eb5a94d57e200d44810d0
Joseph Hill <dezolent@gmail.com>
2025-06-06 23:15:21 -0400

    Updated console-intercept.client.ts

--- plugins/console-intercept.client.ts
+++ plugins/console-intercept.client.ts
@@ -1,25 +1,17 @@
-<script setup lang="ts">
-import { ref, watch, computed } from 'vue'
-import { useShopify } from '~/composables/useShopify'
+// /plugins/console-intercept.client.ts
+import { defineNuxtPlugin } from '#app'
 
-const { getProducts } = useShopify()
-const selectedFilter = ref('all')
-const allProducts   = ref([])
-const pageInfo      = ref(null)
+export default defineNuxtPlugin((nuxtApp) => {
+  // this code only runs in the browser
+  if (process.client) {
+    // set your flag
+    ;(window as any).myFlag = true
 
-const { data: productsData, pending, error, refresh } = 
-  await useAsyncData('featured-products', () => getProducts(4))
-
-watch(productsData, (d) => {
-  if (d?.products) {
-    allProducts.value = d.products.edges
-    pageInfo.value    = d.products.pageInfo
+    // override console.error
+    const origError = console.error.bind(console)
+    console.error = (...args: unknown[]) => {
+      // …your intercept logic here…
+      origError(...args)
+    }
   }
-}, { immediate: true })
-
-// …rest of your computed, methods etc…
-</script>
-  
-<template>
-  <!-- render pending / error / grid exactly like on /products -->
-</template>
+})
